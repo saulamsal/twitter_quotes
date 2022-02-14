b698c325b42697d4da5db66623d132686e21dd5b
saulamsal <saulamsal@gmail.com>
2022-02-13 19:28:50 -0500

    setting up pointers for current arr

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-const http = require("https");
-const MongoClient = require('mongodb').MongoClient;
+import * as https from "https";
+import "dotenv/config";
+import { MongoClient } from "mongodb";
 
-function getQuote() {
-    return new Promise((resolve, reject) => {
-        http.get("https://zenquotes.io/api/quotes", (res) => {
-            let body = "";
-            res.on("data", (chunk) => {
-                body += chunk;
-            }
-            );
-            res.on("end", () => {
-                resolve(body);
-            });
-        });
-    });
-}
+let client = null;
 
-getQuote().then((quotes) => {
-     quotes = JSON.parse(quotes);
-    console.log(quotes[0]);
-}
+MongoClient.connect(
+  process.env.MONGODB_DATABASE_STRING,
+  function (err, client) {
+    if (err) throw err;
+    console.log("Database is connected");
+    var client = client;
+
+    //      db.listCollections({ name: "current_quotes_array" }).toArray(function(err, collections) {
+    //     if (err) throw err;
+    //     if (collections.length === 0) {
+    //         await getQuotesAndStoreInCurrentQuotesArray(db);
+    //     }
+    // });
+  }
 );
 
+let db = client.db(process.env.MONGODB_DATABASE_NAME);
 
+const getQuotesAndStoreInCurrentQuotesArray = async (db) => {
+  getQuote().then((quotes) => {
+    quotes = JSON.parse(quotes);
+    //insert all in current_quotes_array
+    db.collection("current_quotes_array").insertMany(
+      quotes,
+      function (err, res) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+      }
+    );
+  });
+};
+
+function getQuote() {
+  return new Promise((resolve, reject) => {
+    https.get(process.env.API_URL, (res) => {
+      let body = "";
+      res.on("data", (chunk) => {
+        body += chunk;
+      });
+      res.on("end", () => {
+        resolve(body);
+      });
+    });
+  });
+}
+
+client.close();
